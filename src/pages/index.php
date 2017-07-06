4fc3d627a6f3f80a10e1d77fdeb5af681857a0c7
Nico Watine <nico@studioncreations.com>
2017-07-05 21:47:16 -0500

    Create bare-bones contactBar in header

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,13 @@ og-image:
 body-id: index
 body-class:
 ---
+<header>
+	<div class="row expanded contactBar">
+		<div class="medium-6 columns">
+			<a href="mailto:email@williamsmalone.com">email@williamsmalone.com</a>
+		</div>
+		<div class="medium-6 columns text-right">
+			<p>123-456-7891</p>
+		</div>
+	</div>
+</header>
